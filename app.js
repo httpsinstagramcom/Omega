49756e95178f520ba80c934d513cbc7f350bbf40
httpsinstagramcom <77456097+httpsinstagramcom@users.noreply.github.com>
2021-01-14 22:53:17 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,15 +3,16 @@
 
 ///Edit Under This only
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZfEM0q0er5e2HBI9X9NckKORDtz783J8",
+  authDomain: "nobi-d236c.firebaseapp.com",
+  databaseURL: "https://nobi-d236c-default-rtdb.firebaseio.com",
+  projectId: "nobi-d236c",
+  storageBucket: "nobi-d236c.appspot.com",
+  messagingSenderId: "976015869391",
+  appId: "1:976015869391:web:40724addf63c7df1b4beba",
+  measurementId: "G-R74Y3NSNNQ"
 };
 
      
